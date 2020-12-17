c1c7a8b6e7b1f8173cd602ff83ea9b86a73275a3
Julien SEIXAS (MrZyr0) <32927775+MrZyr0@users.noreply.github.com>
2020-12-17 04:25:11 +0100

    refacto: delete testEnvSetupService.js

--- src/tests/helpers/testEnvSetupService.js
+++
@@ -1,7 +0,0 @@
-'use-strict';
-import mongoose from "mongoose";
-
-if ( process.env.TESTS_CLEAR_BD === "true" )
-	for ( const collection in mongoose.connection.collections ) {
-		mongoose.connection.collection(collection).drop();
-	}
